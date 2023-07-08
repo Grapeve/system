d9090d06f9215c6eda088e90aeeecb01b82e459f
Rhys <2068188418@qq.com>
2023-07-08 20:02:37 +0800

    feat(代码规范): esLint + Prettier + husky + lint-staged + cz-git + commitlint

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier/skip-formatting',
+    // 'eslint-config-airbnb-base',
+    'plugin:prettier/recommended'
+  ],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto'
+      }
+    ]
+  }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# 电商管理系统
+
+## 代码规范
+
++ ESLint
+
++ Prettier
+
++ husky + lint-staged
